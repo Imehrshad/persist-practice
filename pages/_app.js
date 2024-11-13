287c3018ff71875a15c284a3789f5e318b2c4aa5
Mehrshad Khatibi <khatibimehrshad24@gmail.com>
2024-11-13 10:55:47 +0330

    searchbar deleted

--- pages/_app.js
+++ pages/_app.js
@@ -10,7 +10,7 @@ export default function App({ Component, pageProps }) {
   return <>
     <Provider store={store}>
       <PersistGate loading={<h1>Loading ... </h1>} persistor={persistedStore}>
-        <MyHeader />
+        {/* <MyHeader /> */}
         <Component {...pageProps} />
         <Footer />
       </PersistGate>
